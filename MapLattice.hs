46c49ca3c8389cb4aeeeafd60aebbe0f2ade4707
Bjarke Bondo Andersen <andersen.bjarke@gmail.com>
2013-02-21 16:09:10 +0100

    Remove newtype wrapping from MapLattice

--- MapLattice.hs
+++ MapLattice.hs
@@ -1,4 +1,4 @@
-module MapLattice (MapLattice(..)) where
+module MapLattice () where
 
 import Lattice
 
@@ -6,20 +6,17 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Prelude hiding (pred, succ)
 
-newtype MapLattice k v = MapLattice (Map k v) deriving (Eq, Ord)
-
-instance (Ord k, Lattice v) => Lattice (MapLattice k v) where
-  MapLattice a `leq` MapLattice b =
-    if Map.null a || Map.keys a /= Map.keys b
-    then Nothing
-    else
-      let (v:vs) = zipWith leq (Map.elems a) (Map.elems b) in
-      if all (v==) vs then v else Nothing
+instance (Ord k, Lattice v) => Lattice (Map k v) where
+  a `leq` b = if Map.null a || Map.keys a /= Map.keys b
+              then Nothing
+              else
+                let (v:vs) = zipWith leq (Map.elems a) (Map.elems b) in
+                if all (v==) vs then v else Nothing
   pred = concatAdjust pred
   succ = concatAdjust succ
-  top (MapLattice m) = MapLattice $ Map.map top m
-  bottom (MapLattice m) = MapLattice $ Map.map bottom m
+  top m = Map.map top m
+  bottom m = Map.map bottom m
 
-concatAdjust :: (Ord k, Lattice v) => (v -> [v]) -> MapLattice k v -> [MapLattice k v]
-concatAdjust ite (MapLattice m) =
-  concatMap (\(k,v) -> [ MapLattice $ Map.adjust (const p) k m | p <- pred v ]) $ Map.assocs m
+concatAdjust :: (Ord k, Lattice v) => (v -> [v]) -> Map k v -> [Map k v]
+concatAdjust ite m =
+  concatMap (\(k,v) -> [ Map.adjust (const p) k m | p <- pred v ]) $ Map.assocs m
