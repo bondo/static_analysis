b6fc9eed2b85e305469e57735bda2ad201e7f482
Bjarke Bondo Andersen <andersen.bjarke@gmail.com>
2013-02-21 12:49:43 +0100

    Make LUB and GLB part of the typeclass

--- Lattice.hs
+++ Lattice.hs
@@ -1,4 +1,4 @@
-module Lattice where
+module Lattice (Lattice(..)) where
 
 import Data.List
 --import Data.Maybe
@@ -10,6 +10,15 @@ class Lattice e where
   succ :: e -> [e]
   top :: e
   bottom :: e
+  
+  -- Ord instances are usefull for sorting, they will usually be
+  -- derived and have no relation to leq
+  leastUpperBound :: Ord e => e -> e -> e
+  greatestLowerBound :: Ord e => e -> e -> e
+
+  -- Default to transitive closure implementations
+  leastUpperBound = leastUpperBound'
+  greatestLowerBound = greatestLowerBound'
 
 closure :: Ord e => (e -> [e]) -> e -> [e]
 closure ite x = map head . group . sort . (x:) . concatMap (closure ite) $ ite x
@@ -25,15 +34,15 @@ intersection as@(a:as') bs@(b:bs') = case a `compare` b of
 closureIntersection :: Ord e => (e -> [e]) -> e -> e -> [e]
 closureIntersection ite a b = intersection (closure ite a) (closure ite b)
 
-leastUpperBound :: (Ord e, Lattice e) => e -> e -> e
-leastUpperBound a b = foldl1' folder $ closureIntersection succ a b
+leastUpperBound' :: (Ord e, Lattice e) => e -> e -> e
+leastUpperBound' a b = foldl1' folder $ closureIntersection succ a b
   where folder a b = case a `leq` b of
           Just True  -> a
           Just False -> b
           Nothing    -> error "Lattice.LUB: Precondition failed"
 
-greatestLowerBound :: (Ord e, Lattice e) => e -> e -> e
-greatestLowerBound a b = foldl1' folder $ closureIntersection pred a b
+greatestLowerBound' :: (Ord e, Lattice e) => e -> e -> e
+greatestLowerBound' a b = foldl1' folder $ closureIntersection pred a b
   where folder a b = case a `leq` b of
           Just True  -> a
           Just False -> b
